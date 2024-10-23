4ee1015906b440a8285a3f74cea41346d5574d2e
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2024-10-23 20:15:22 -0300

    jaaj

---
+++ quit-smoking/app/registro/page.tsx
@@ -0,0 +1,185 @@
+"use client";
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    birthYear: '',
+    gender: ''
+  });
+
+  const router = useRouter();  // Para la navegación
+  const handleBack = () => {
+    router.back();  // Regresa a la página anterior
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Form data submitted:', formData);
+    // Aquí puedes agregar la lógica para manejar el registro de usuarios
+  };
+
+  return (
+    <div style={styles.wrapper}>
+      {/* Flecha para regresar */}
+      <button onClick={handleBack} style={styles.backButton}>
+        ← 
+      </button>
+
+      <img
+        src="/vector.png"  // Imagen izquierda
+        alt="Decoración izquierda"
+        style={styles.imageLeft}
+      />
+      <div style={styles.registerContainer}>
+        <h2>Ingresa tus datos para que tu vida sin humo comience hoy</h2>
+        <form onSubmit={handleSubmit} style={styles.registerForm}>
+          <div style={styles.inputGroup}>
+            <input 
+              type="text" 
+              name="firstName" 
+              placeholder="Nombre..." 
+              value={formData.firstName} 
+              onChange={handleChange} 
+              style={styles.input}
+              required 
+            />
+            <input 
+              type="text" 
+              name="lastName" 
+              placeholder="Apellido..." 
+              value={formData.lastName} 
+              onChange={handleChange} 
+              style={styles.input}
+              required 
+            />
+          </div>
+
+          <div style={styles.inputGroup}>
+            <input 
+              type="email" 
+              name="email" 
+              placeholder="Correo electronico..." 
+              value={formData.email} 
+              onChange={handleChange} 
+              style={styles.input}
+              required 
+            />
+            <input 
+              type="password" 
+              name="password" 
+              placeholder="Contraseña..." 
+              value={formData.password} 
+              onChange={handleChange} 
+              style={styles.input}
+              required 
+            />
+          </div>
+
+          <div style={styles.inputGroup}>
+            <input 
+              type="number" 
+              name="birthYear" 
+              placeholder="Año de nacimiento..." 
+              value={formData.birthYear} 
+              onChange={handleChange} 
+              style={styles.input}
+              required 
+            />
+            <input 
+              type="text" 
+              name="gender" 
+              placeholder="Genero..." 
+              value={formData.gender} 
+              onChange={handleChange} 
+              style={styles.input}
+            />
+          </div>
+
+          <button type="submit" style={styles.button}>REGISTRARSE</button>
+        </form>
+      </div>
+      <img
+        src="/imagen.png"  // Imagen derecha
+        alt="Decoración derecha"
+        style={styles.imageRight}
+      />
+    </div>
+  );
+};
+
+// Estilos inline
+const styles = {
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative' as 'relative',
+  },
+  backButton: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+    fontSize: '44px',
+    color: '#F7931E',
+  },
+  registerContainer: {
+    backgroundColor: '#e0e0e0',
+    padding: '20px',
+    borderRadius: '10px',
+    maxWidth: '400px',
+    textAlign: 'center' as 'center',
+    zIndex: 1,
+  },
+  registerForm: {
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    gap: '20px',
+  },
+  inputGroup: {
+    display: 'flex',
+    justifyContent: 'space-between' as 'space-between',
+  },
+  input: {
+    padding: '10px',
+    width: '48%',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+  },
+  button: {
+    padding: '15px',
+    backgroundColor: '#000',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '16px',
+  },
+  imageLeft: {
+
+    position: 'absolute',
+  top: '18%',
+  left: '11%',
+  width: '200px', 
+  height: 'auto',
+  },
+  imageRight: {
+    position: 'absolute' as 'absolute',
+    right: '0',
+    top: '100%',
+    transform: 'translateY(-50%)',
+    zIndex: 0,
+  },
+};
+
+export default RegisterForm;
