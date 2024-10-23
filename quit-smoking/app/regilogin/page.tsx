c35eb26be7706bf1fea707fc3e78968887464f92
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2024-10-23 11:22:53 -0300

    jsnamxcmP

--- quit-smoking/app/regilogin/page.tsx
+++ quit-smoking/app/regilogin/page.tsx
@@ -2,6 +2,72 @@
 
 import React from 'react';
 
+// Define los estilos con el tipo correcto
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    position: 'relative',
+    backgroundColor: 'white',
+    fontFamily: 'Arial, sans-serif',
+  },
+  backArrow: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+    fontSize: '44px',
+    color: '#f49034',
+    background: 'none', // Sin fondo
+    border: 'none', // Sin borde
+    padding: 0, // Sin padding
+},
+  sideImageLeft: {
+    position: 'absolute',
+    top: '44px',
+    left: '20px',
+    width: '150px',
+
+  },
+  sideImageRight: {
+    position: 'absolute',
+    maxWidth: '100%',
+    width: '150px',
+    height: 'auto',
+    right: '0px',
+    top: '270px',
+  },
+  logo: {
+    marginBottom: '20px',
+  },
+  logoImage: {
+    width: '160px',
+  },
+  loginBtn: {
+    width: '240px',
+    padding: '15px',
+    fontSize: '18px',
+    margin: '10px 0',
+    border: 'none',
+    cursor: 'pointer',
+    backgroundColor: '#f68b1f',
+    color: 'black',
+  },
+  registerBtn: {
+    width: '240px',
+    padding: '15px',
+    fontSize: '18px',
+    margin: '10px 0',
+    border: 'none',
+    cursor: 'pointer',
+    backgroundColor: 'black',
+    color: '#f68b1f',
+  }
+};
+
 const App = () => {
   // Función para regresar a la página anterior
   const goBack = () => {
@@ -44,70 +110,5 @@ const App = () => {
     </div>
   );
 }
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    position: 'relative',
-    backgroundColor: 'white',
-    fontFamily: 'Arial, sans-serif',
-  },
-  backArrow: {
-    fontSize: '24px',
-    cursor: 'pointer',
-    position: 'absolute', // Posiciona la flecha de manera absoluta
-    top: '20px', // Espaciado desde la parte superior
-    left: '20px', // Espaciado desde la izquierda
-    zIndex: 10, // Asegura que la flecha esté por encima de otros elementos
-  },
-  sideImageLeft: {
-    position: 'absolute',
-    top: '20px',
-    left: '20px',
-    fontSize: '44px',
-    color: '#f49034',
-  },
-  sideImageRight: {
-    position: 'absolute',
-    margin: '20px auto',
-    maxWidth: '100%',
-    width: '150px',
-    height: 'auto',
-    paddingLeft: '900px',
-    right: '0px',
-    bottom: '0px',
-    top: '270px',
-  },
-  logo: {
-    marginBottom: '20px',
-  },
-  logoImage: {
-    width: '160px',
-  },
-  loginBtn: {
-    width: '200px',
-    padding: '15px',
-    fontSize: '18px',
-    margin: '10px 0',
-    border: 'none',
-    cursor: 'pointer',
-    backgroundColor: '#f68b1f',
-    color: 'black',
-  },
-  registerBtn: {
-    width: '200px',
-    padding: '15px',
-    fontSize: '18px',
-    margin: '10px 0',
-    border: 'none',
-    cursor: 'pointer',
-    backgroundColor: 'black',
-    color: '#f68b1f',
-  }
-};
 
 export default App;
