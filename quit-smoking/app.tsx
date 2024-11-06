56c598e9ebcdddfb997ed3e7c6a80a9624164026
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2024-11-06 12:25:05 -0300

    aa

---
+++ quit-smoking/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './Login'; // Ajusta la ruta si es necesario
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+};
+
+export default App;
